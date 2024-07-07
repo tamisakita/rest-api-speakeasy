97811b7036935c2fd63d2355c0791abaef838bea
Patricia Tami Sakita <tamisakita@hotmail.com>
2024-07-06 23:31:49 -0700

    refactored code

--- index.js
+++ index.js
@@ -4,82 +4,19 @@ const express = require("express");
 const mongoose = require("mongoose");
 const Phrases = require("./models/phrase.model.js");
 
+const airportPhrasesRoute = require("./routes/airportphrase.route.js");
 // framework to handle the requests
 const app = express();
 
-// make the return into json format
+// middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// routes
+app.use("/airport", airportPhrasesRoute);
 
 app.get("/", (req, res) => {
   res.send("Hellooo GET dddd");
-});
-
-// retrieve all phrases
-app.get("/api/phrases", async (req, res) => {
-  try {
-    const phrases = await Phrases.find({});
-    res.status(200).json(phrases);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-// retrieve only one phrase
-app.get("/api/phrase/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const phrase = await Phrases.findById(id);
-    res.status(200).json(phrase);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-// Insert a Phrase
-app.post("/api/phrases", async (req, res) => {
-  try {
-    const phrases = await Phrases.create(req.body);
-    res.status(200).json(phrases);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-// Update phrase
-app.put("/api/phrase/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-
-    const phrase = Phrases.findByIdAndUpdate(id, req.body);
-
-    // verifying if the phrase exist
-    if (!phrase) {
-      return res.status(404).json({ message: "Phrase not found" });
-    }
-
-    // search the phrase by Id again just to make sure it was updated
-    const updatedPhrase = await Phrases.findById(id);
-
-    res.status(200).json(updatedPhrase);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-// Delete a Phrase
-app.delete("/api/phrase/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const phrase = await Phrases.findByIdAndDelete(id);
-
-    if (!phrase) {
-      return res.status(404).json({ message: "Phrase not found" });
-    }
-
-    res.status(200).json({ message: "Phrase deleted successfully" });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
 });
 
 // Connection to mongo DB and running the server on port 3000
